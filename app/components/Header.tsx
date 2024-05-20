f4e789381e1ad14d5b9ea2779d1aa0fe52cf2cc2
Phillip Nguyen <png9981@gmail.com>
2024-05-20 22:07:28 +0700

    feat: update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,13 @@
 import { Link } from "@remix-run/react";
+import AvatarUrl from '~/assets/images/png_portraits.jpeg'
 
 export default function Header() {
   return (
     <header className="flex justify-between">
-      <Link to="/">
-        <h1 className="text-2xl font-bold">{"Saldyy's blog"}</h1>
+      <Link to="/" className="text-primary">
+        <h1 className="text-2xl font-bold ">{"Saldyy's blog"}</h1>
       </Link>
-      <h1 className="text-2xl font-bold">Phillip</h1>
+      <img src={AvatarUrl} alt="avatar" className="w-12 h-12 rounded-full" />
     </header>
   )
 }
