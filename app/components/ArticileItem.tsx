4934d625f8e45c98d39840777b461aac59288a53
Phillip Nguyen <png9981@gmail.com>
2024-05-21 08:15:40 +0700

    fix: wrong navigation

--- app/components/ArticileItem.tsx
+++ app/components/ArticileItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@remix-run/react";
 import { ArticleFrontMatter } from "~/types";
 import { getDisplayDate } from "~/utils";
 
@@ -7,12 +8,12 @@ export type ArticleItemProps = {
 export default function ArticleItem({ data }: ArticleItemProps) {
   const { title, summary, createdAt, slug } = data
   return (
-    <a href={`/articles/${slug}`}>
+    <Link to={`/articles/${slug}`}>
       <article className="w-100 mb-5">
         <h1 className="text-2xl text-primary font-bold">{title}</h1>
         <p className="text-sm">{getDisplayDate(new Date(createdAt))}</p>
         <p className="text-lg">{summary}</p>
       </article>
-    </a>
+    </Link>
   );
 }
