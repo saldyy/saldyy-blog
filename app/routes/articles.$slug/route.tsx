ff5a200f6eb2773385e3b1fb629a0e2531606881
Phillip Nguyen <png9981@gmail.com>
2024-05-22 16:05:26 +0700

    feat: update meta for article page

--- app/routes/articles.$slug/route.tsx
+++ app/routes/articles.$slug/route.tsx
@@ -1,8 +1,16 @@
-import { json, useLoaderData, } from "@remix-run/react"
+import { MetaFunction, json, useLoaderData, } from "@remix-run/react";
 import Markdown from "react-markdown"
 import { getArticlesBySlug } from "~/.server/data/articles"
+import { Article, ArticleFrontMatter } from "~/types";
 import { getDisplayDate } from "~/utils"
 
+export const meta: MetaFunction = ({ data }) => {
+  const articleFrontMatter = (data as Article).data as ArticleFrontMatter
+  return [
+    { title: articleFrontMatter.title },
+    { name: articleFrontMatter.title, content: articleFrontMatter.summary },
+  ];
+};
 
 export async function loader({ params }: { params: { slug: string } }) {
   const { slug } = params
@@ -18,10 +26,10 @@ export default function ArticlePage() {
     <main className="w-100 mt-5">
       <div className="mb-5">
         <h1 className="text-4xl font-bold mb-2">Article</h1>
-        <p>{getDisplayDate(new Date(data.createdAt)) }</p>
+        <p>{getDisplayDate(new Date(data.createdAt))}</p>
       </div>
       <div className="prose">
-      <Markdown>{content}</Markdown>
+        <Markdown>{content}</Markdown>
       </div>
     </main>
   )
